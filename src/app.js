812cfa2680e0b91775b69f9b7a66e080717325c0
Jonathan vera <167922770+jonathanavl@users.noreply.github.com>
2024-06-05 11:33:20 +0000

    domain

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "it", "he", "she"];
+let adj = ["great", "big", "acid", "bad", "cool"];
+let noun = ["jogger", "racoon", "developer", "nike", "card"];
+let ext = [".com", ".net", ".us", ".io", ".es"];
+
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let e = 0; e < ext.length; e++) {
+        let domain = pronoun[p] + adj[a] + noun[n] + ext[e];
+        console.log(domain);
+      }
+    }
+  }
+}
